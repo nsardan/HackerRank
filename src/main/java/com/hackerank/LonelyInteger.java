3544eec42a2eb1ba98b8341882fa7a5df32276c2
nsardana <neha.nsit@gmail.com>
2017-07-31 01:46:23 -0400

    Using bitwise xor to find lonely integer inbetween duplicates

---
+++ src/main/java/com/hackerank/LonelyInteger.java
@@ -0,0 +1,24 @@
+package com.hackerank;
+
+import java.util.Scanner;
+
+public class LonelyInteger {
+    static int lonelyinteger(int[] a) {
+        int ans = 0;
+        for(int i = 0; i< a.length; i++){
+            ans = ans ^ a[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i = 0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int result = lonelyinteger(a);
+        System.out.println(result);
+    }
+}
