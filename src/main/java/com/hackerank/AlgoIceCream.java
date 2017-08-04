46c947c3f29d2692c2023015089f4751e3f8f9e9
nsardana <neha.nsit@gmail.com>
2017-08-03 23:41:41 -0400

    Ice cream problem in Hackerank

--- src/main/java/com/hackerank/AlgoIceCream.java
+++ src/main/java/com/hackerank/AlgoIceCream.java
@@ -15,52 +15,30 @@ public class AlgoIceCream {
 
         Scanner scanner = new Scanner(System.in);
         int nTrip = scanner.nextInt();
+        scanner.nextLine();
         for(int i = 0; i < nTrip; i ++){
            int mDollar = scanner.nextInt();
            int nIceCreams = scanner.nextInt();
            int iceCreamsCostArray[] = new int[nIceCreams];
-           for(int j = 0; i< nIceCreams; i++){
+           for(int j = 0; j < nIceCreams; j++){
                iceCreamsCostArray[j] = scanner.nextInt();
            }
             calculateCostIceCreamForEach(iceCreamsCostArray, mDollar);
-/*
-           if(minMax.get(0)> minMax.get(1)){
-               System.out.println(minMax.get(1)+ " " +minMax.get(0));
-           }else
-           {
 
-               System.out.println(minMax.get(0)+ " " +minMax.get(1));
-           }
-*/
        }
-      // scanner.close();
 
     }
 
     private static void calculateCostIceCreamForEach(int[] iceCreamsCostArray, int maxCost) {
 
-      /*  int min = 0;
         for(int i = 0; i< iceCreamsCostArray.length; i++){
-            int a = iceCreamsCostArray[i];
-            for(int j = i; j< iceCreamsCostArray.length; j++){
-                if(a + iceCreamsCostArray[j] == maxCost){
-
+            int temp = iceCreamsCostArray[i];
+            for(int j= i+1; j < iceCreamsCostArray.length; j++){
+                if(maxCost == (temp + iceCreamsCostArray[j])){
+                    System.out.println((i+1) + " "+ (j+1));
                 }
+
             }
         }
-        return null;*/
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(iceCreamsCostArray[0], 1);
-
-        for(int i = 1; i< iceCreamsCostArray.length; i++){
-            Integer an = map.get(maxCost - iceCreamsCostArray[i]);
-            if(an == null){
-                map.put(iceCreamsCostArray[i], i+1);
-            }else{
-                System.out.print(i+1 + " ");
-            }
-        }
-
-       // System.out.println("NEHA");
     }
 }
